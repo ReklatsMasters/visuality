95797c0d8e81161ceb230246d9aa695375d9a9bf
ReklatsMasters <dmitrycvet@gmail.com>
2015-12-20 06:50:26 +0500

    nixpal: suppport multiple nodes, support navigation

--- src/mixpal.js
+++ src/mixpal.js
@@ -1,32 +1,47 @@
-import waveform from '../lib/waveform';
+/* global History */
+
+import {FrequencyAnalyser} from '../lib/analyser';
 import {ColumnChart} from '../lib/chart';
 
-const id = 'jp_audio_0';
-const audio = document.getElementById(id);
+let id = ['jp_audio_0', 'jp_audio_1'];
+let audios = id.map(e => document.getElementById(e));
 
-const rootNode = makeWrapper();
+const rootNode = visualizerNode();
+insertNode(rootNode);
+
+// this version history.js does not trigger `popstate` event correctly
+History.Adapter.bind(window, 'statechange', () => insertNode(rootNode));
+
 const width = parseInt(getComputedStyle(rootNode).width);
 
-const chart = new ColumnChart(rootNode, {width});
+const analyser = new FrequencyAnalyser(visualize);
+const chart = new ColumnChart(rootNode, { width, dataLength: analyser.dataCount });
 
 function visualize(uint8data) {
 	chart.draw(Array.from(uint8data).map((y, x) => ({ x, y })));
 }
 
-// обрабатываем данные при проигрывании
-audio.addEventListener('canplay', ev => {
-	waveform(ev.target).start(visualize);
-});
+audios.forEach(audio => {
+  analyser.add(audio);
+
+  audio.addEventListener('canplay', function oncanplay() {
+    analyser.disconnect();
+    analyser.connect(audio);
+  });
+})
 
 /* создаёт блок-обёртку в котором будет располагаться визуализатор */
-function makeWrapper() {
-	var anchor = document.querySelector('.carusel_block');
-
+function visualizerNode() {
 	var wrapper = document.createElement('div');
 	wrapper.classList.add('visualizer_block');
 	wrapper.style.marginTop = "20px";
 
-	anchor.parentNode.insertBefore(wrapper, anchor.nextElementSibling);
-
 	return wrapper;
+}
+
+function insertNode(node) {
+  if (document.contains(node)) { return; }
+  
+  var anchor = document.querySelector('.carusel_block');
+  anchor.parentNode.insertBefore(node, anchor.nextElementSibling);
 }
